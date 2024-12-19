4ca223790102c0e60c5a8e359ce73958f995c4e5
RahmanAdel <rahmanadel619@gmail.com>
2024-12-19 19:29:42 +0600

    Making improvements

--- Novulyn/script.js
+++ Novulyn/script.js
@@ -1,31 +1,3 @@
-// Card Animation Logic
-const cards = document.querySelectorAll('.brainchild');
-const threshold = 200; // Increased threshold
-let isScrolling = false;
-
-function revealCards() {
-    if (isScrolling) return;
-    isScrolling = true;
-
-    requestAnimationFrame(() => {
-        cards.forEach((card) => {
-            const cardTop = card.getBoundingClientRect().top;
-            const cardBottom = card.getBoundingClientRect().bottom; // Added cardBottom
-            const windowBottom = window.innerHeight || document.documentElement.clientHeight;
-            if (cardTop - windowBottom < threshold) {
-                card.classList.add("card");
-            }
-            else if (cardTop > windowBottom || cardBottom < 0) {
-                card.classList.remove("card");
-            }
-        });
-        isScrolling = false;
-    });
-}
-
-window.addEventListener('scroll', revealCards);
-revealCards();
-
 // Text Animation Logic
 var typed = new Typed("#typed-output", {
     strings: [
