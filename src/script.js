69abffbb97cfcb7d68112b727d759d713c012c96
RahmanAdel <rahmanadel619@gmail.com>
2025-01-05 09:59:20 +0600

    Final changes until 09/01/2025

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // Text Animation Logic
 var typed = new Typed("#typed-output", {
     strings: [
-        "I don't confine myself to a single title -<br> I am a problem solver, innovator, and creator.",
+        "I don't confine myself to a single title <br>- I am a problem solver, innovator, and creator.",
         "Bridging vision and technology is my expertise,<br> enabling the transformation of ideas into impactful realities."
     ],
     typeSpeed: 40,          // Slightly faster typing
